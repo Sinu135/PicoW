48a7e05a1760a9de7ded6fcc69c5009e072f6273
AlvarezKevinDaniel <85995516+AlvarezKevinDaniel@users.noreply.github.com>
2024-05-21 17:12:01 -0700

    Add files via upload

---
+++ practica1/practica1/practica1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace practica1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.txtBoxMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(161, 55);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Conectar";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(242, 55);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(82, 23);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Desconectar";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 164);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(109, 164);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(12, 57);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorts.TabIndex = 4;
+            this.comboBoxPorts.SelectedIndexChanged += new System.EventHandler(this.comboBoxPorts_SelectedIndexChanged);
+            // 
+            // txtBoxMessage
+            // 
+            this.txtBoxMessage.Location = new System.Drawing.Point(12, 124);
+            this.txtBoxMessage.Name = "txtBoxMessage";
+            this.txtBoxMessage.Size = new System.Drawing.Size(172, 20);
+            this.txtBoxMessage.TabIndex = 5;
+            this.txtBoxMessage.TextChanged += new System.EventHandler(this.txtBoxMessage_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::practica1.Properties.Resources.gato;
+            this.ClientSize = new System.Drawing.Size(352, 213);
+            this.Controls.Add(this.txtBoxMessage);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.TextBox txtBoxMessage;
+    }
+}
+
---
+++ practica1/practica1/practica1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace practica1
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPorts();
+        }
+        private void InitializeSerialPorts()
+        {
+            comboBoxPorts.Items.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                comboBoxPorts.Items.Add($"COM{i}");
+            }
+            comboBoxPorts.SelectedIndex = 0;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtBoxMessage.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort != null && serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+
+                string selectedPort = comboBoxPorts.SelectedItem.ToString();
+                serialPort = new SerialPort(selectedPort, 9600);
+                serialPort.Open();
+                MessageBox.Show("Conectado al puerto " + selectedPort);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                MessageBox.Show("Desconectado del puerto " + serialPort.PortName);
+            }
+            else
+            {
+                MessageBox.Show("No hay conexión activa para desconectar.");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                string message = txtBoxMessage.Text;
+                serialPort.WriteLine(message);
+                MessageBox.Show("Mensaje enviado: " + message);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, conecta al puerto serial primero.");
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+        private void txtBoxMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
